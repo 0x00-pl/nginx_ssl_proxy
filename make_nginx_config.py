cb626f1a5741176055440464ddaa79781ad02950
0x00-pl <0x00.pl@gmail.com>
2017-12-25 19:44:02 +0800

    add deployment_config support

--- make_nginx_config.py
+++ make_nginx_config.py
@@ -1,4 +1,7 @@
 import os
+import json
+
+cwd = os.path.dirname(os.path.realpath(__file__))
 
 def get_file(template, cert_key_path, localhost, server_name='~^.*$'):
   template = template.replace('{{cert_chain.crt}}', cert_key_path[0])
@@ -14,11 +17,32 @@ def gen_cert_chain(base_dir):
   path = [os.path.join(cwd, 'cert_chain.crt'), os.path.join(cwd, 'private.key')]
   return path
 
-def main(base_dir, localhost):
+def main(base_dir, localhost, site_name):
   path = gen_cert_chain(base_dir)
-  template = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'ssl_proxy.example')).read()
-  conf = get_file(template, path, localhost)
+  print(__file__)
+  print(os.path.realpath(__file__))
+  template = open(os.path.join(cwd, 'ssl_proxy.example')).read()
+  conf = get_file(template, path, localhost, site_name)
   print(conf)
 
+def load_config():
+  dep_path = os.path.join(cwd, 'deployment_config')
+  dep_conf_path = os.path.join(dep_path, 'ssl_proxy.json')
+  j = {
+    'ssl_path': os.path.join(dep_path, 'sslforfree'),
+    'site_name': '~^.*$',
+    'proxy_url': 'http://localhost:8080'
+  }
+  try:
+    fj = json.load(open(dep_conf_path))
+    j["ssl_path"] = fj.get("ssl_path", j["ssl_path"])
+    j["site_name"] = fj.get("site_name", j["site_name"])
+    j["proxy_url"] = fj.get("proxy_url", j["proxy_url"])
+  except e:
+    pass
+
+  return j
+
 if __name__ == '__main__':
-  main(os.getcwd(), 'http://localhost:8080')
+  conf = load_config()
+  main(conf["ssl_path"], conf["proxy_url"], conf["site_name"])
